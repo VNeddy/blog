c0a88bb1fc8aff39df292cac10d37e873086a9a2
vneddy <vneddychen@gmail.com>
2017-03-09 15:48:42 +0800

    修改后台侧边栏

--- src/admin/includes/sidebar.inc.php
+++ src/admin/includes/sidebar.inc.php
@@ -10,11 +10,11 @@ if (!defined('IN_TG')) {
     <h3><a href="index.php">后台管理</a></h3>
     <ul>
         <li><a href="article.manage.php">管理文章</a></li>
+        <li><a href="article.manage.php">管理文章评论</a></li>
         <li><a href="article.add.php">发布文章</a></li>
         <li><a href="gossip.manage.php">管理说说</a></li>
         <li><a href="gossip.add.php">发布说说</a></li>
         <li><a href="photo.manage.php">管理照片</a></li>
-        <li><a href="photo.add.php">发布照片</a></li>
         <li><a href="message.manage.php">管理留言</a></li>
         <li><a href="about.manage.php">管理个人简介</a></li>
     </ul>
